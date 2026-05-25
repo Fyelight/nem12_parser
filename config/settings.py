741c31b4c09647217b34eef2c02ae3b9dee982f1
Fyelight <kaid.fyelight@hotmail.com>
2026-05-25 22:32:32 +0800

    clean up output files and add gitignore for output files

--- config/settings.py
+++ config/settings.py
@@ -7,16 +7,16 @@ class Settings(BaseSettings):
 
     # File path for testing
     input_folder_path: str = "tests/nem12_csv"
-    output_folder_path: str = "sql_chunks"
-    state_file_path: str = "state.txt"
-    completed_files_tracker: str = "processed_files.log"
+    output_folder_path: str = "output/sql_chunks"
+    state_file_path: str = "output/state.txt"
+    completed_files_tracker: str = "output/processed_files.log"
 
     batch_size: int = 5000
     source_type: str = "file"
 
     # Dead-Letter Queue Output Files
-    dlq_report_csv: str = "corrupted_lines.csv" # For human analytics review
-    dlq_replay_csv: str = "replay_failed_records.csv" # raw lines ready to be re-run later after fixing errors
+    dlq_report_csv: str = "output/corrupted_lines.csv" # For human analytics review
+    dlq_replay_csv: str = "output/replay_failed_records.csv" # raw lines ready to be re-run later after fixing errors
 
     model_config = SettingsConfigDict(
         env_file=".env",
